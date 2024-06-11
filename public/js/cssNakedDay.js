f0ec599b9ee5d309cf4ee41be221a7b6495c4e6e
Ari Prakash <ariprakash16@gmail.com>
2024-06-11 12:28:16 -0400

    configurable css naked day and tested now so it works

--- public/js/cssNakedDay.js
+++ public/js/cssNakedDay.js
@@ -1,12 +1,14 @@
+const cssNakedDay = "04-09";
+
 const now = new Date();
 const currentYear = now.getFullYear();
 
-const start = new Date(`${currentYear}-04-09T00:00:00+1400`).getTime();
-const end = new Date(`${currentYear}-04-10T00:00:00-1200`).getTime();
+const start = new Date(`${currentYear}-${cssNakedDay}T00:00:00+1400`).getTime();
+const end = new Date(`${currentYear}-${cssNakedDay}T23:59:59-1200`).getTime();
 
 const currentTime = now.getTime();
 
-if (currentTime >= start && currentTime < end) {
+if (currentTime >= start && currentTime <= end) {
     document
         .querySelectorAll("style, link[rel=stylesheet]")
         .forEach((el) => el.parentElement?.removeChild(el));
