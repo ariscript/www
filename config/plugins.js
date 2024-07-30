45ca65507368b8abba0d0097429b2d7dda09d364
Ari Prakash <ariprakash16@gmail.com>
2024-07-29 22:24:30 -0400

    famous last words: i didnt need these dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,11 @@
 import highlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import rss from "@11ty/eleventy-plugin-rss";
 import bundle from "@11ty/eleventy-plugin-bundle";
-import nav from "@11ty/eleventy-navigation";
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function plugins(config) {
     config.addPlugin(rss);
     config.addPlugin(highlight, { preAttributes: { tabIndex: 0 } });
-    config.addPlugin(nav);
     config.addPlugin(EleventyHtmlBasePlugin);
     config.addPlugin(bundle);
 }
