219c9f9ac2a56fe3a52528b25700ddc75307d542
Ari Prakash <ariprakash16@gmail.com>
2024-12-04 23:15:07 -0500

    icons please work?

--- public/js/webicon.js
+++ public/js/webicon.js
@@ -2,25 +2,29 @@ const DARK_OVERRIDE = {
     "github.com": "https://github.githubassets.com/favicons/favicon-dark.svg",
 };
 
-for (const extLink of document.querySelectorAll(
-    'a[data-external-link="true"]',
-)) {
-    try {
-        const url = new URL(extLink.getAttribute("href"));
+// Don't do icons on localhost because:
+// a) spams a lot of requests to the API
+// b) CORS is annoying and I don't want to deal with it
+if (window.location.hostname !== "localhost") {
+    for (const extLink of document.querySelectorAll(
+        'a[data-external-link="true"]',
+    )) {
+        try {
+            const url = new URL(extLink.getAttribute("href"));
 
-        if (url.hostname in DARK_OVERRIDE) {
-            extLink.style.setProperty(
-                "--webicon-dark",
-                `url("${DARK_OVERRIDE[url.hostname]}")`,
+            if (url.hostname in DARK_OVERRIDE) {
+                extLink.style.setProperty(
+                    "--webicon-dark",
+                    `url("${DARK_OVERRIDE[url.hostname]}")`,
+                );
+            }
+
+            const iconUrl = `https://webicon.ariscript.org/${encodeURIComponent(url.toString())}`;
+            fetch(iconUrl).then(
+                (r) =>
+                    r.status === 200 &&
+                    extLink.style.setProperty("--webicon", `url("${iconUrl}")`),
             );
-        }
-
-        const iconUrl = `https://webicon.ariscript.org/${encodeURIComponent(
-            `${url.protocol}//${url.hostname}`,
-        )}`;
-        fetch(iconUrl).then(
-            (r) =>
-                r.ok && extLink.style.setProperty("--webicon", `url("${url}")`),
-        );
-    } catch {} // if the href is not valid, do nothing
+        } catch {} // if the href is not valid, do nothing
+    }
 }
