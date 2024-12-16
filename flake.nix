0e340a4152e123f296d778669485d02dbea65781
Ari Prakash <ariprakash16@gmail.com>
2024-12-16 12:32:23 -0500

    nix.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { pkgs, system, ... }: {
+        # This sets `pkgs` to a nixpkgs with allowUnfree option set.
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "scratch-devshell"; # Derivation name
+          packages = with pkgs; [    # Executables to include in the devshell
+            nodejs_22
+            git
+          ];
+          inputsFrom = [];           # Include these derivations' dependencies
+          shellHook = "";            # The shellhook to run
+        };
+      };
+    };
+}
