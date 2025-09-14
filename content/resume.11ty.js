f5345c0e783ff7e70637812c2ddfc3b3d2bf512f
Ari Prakash <git@ari.foo>
2025-09-14 13:34:05 -0400

    we have macros at home

---
+++ content/resume.11ty.js
@@ -0,0 +1,26 @@
+export default class ResumeScript {
+    data() {
+        return {
+            eleventyExcludeFromCollections: true,
+            mdTemplateEngine: false,
+            htmlTemplateEngine: false,
+            permalink: "/js/resume.js",
+        };
+    }
+
+    render({ config }) {
+        return `
+            const toggle = document.getElementById("resume-toggle");
+
+            toggle.addEventListener("click", (e) => {
+            if (e.target.checked) {
+                document.body.classList.add("resume");
+                document.title = "Résumé | ${config.author.name}";
+            } else {
+                document.body.classList.remove("resume");
+                document.title = "CV | ${config.author.name}";
+            }
+            });
+        `;
+    }
+}
--- js/resume.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.getElementById("resume-toggle");
-
-toggle.addEventListener("click", (e) =>
-    document.body.classList[e.target.checked ? "add" : "remove"]("resume"),
-);
